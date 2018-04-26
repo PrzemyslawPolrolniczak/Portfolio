4eb418aa8d669fba09463221eaf62dfa13614ea2
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2018-04-26 21:20:23 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <navbar-component></navbar-component>
+    <div class="container">
+        <page-heading-component></page-heading-component>
+        <staff-list-component></staff-list-component>
+    </div>
+  </div>
+</template>
+
+<script>
+  import PageHeadingComponent from './PageHeadingComponent.vue'
+  import StaffListComponent from './StaffListComponent.vue'
+  import NavbarComponent from './NavbarComponent.vue'
+
+  export default {
+    name: 'app',
+    components: {NavbarComponent, PageHeadingComponent, StaffListComponent},
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+body {
+  padding-left: 150px;
+  background-color: #2c3e50;
+  // background-color: #7C8B95
+}
+#app {
+  // font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e2e2e2;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/NavbarComponent.vue
@@ -0,0 +1,52 @@
+<!--NavbarComponent.vue-->
+<template>
+    <nav>
+        <div class="tab">Intro<i class="fas fa-home"></i></div>
+        <div class="tab">O mnie<i class="fas fa-user-circle"></i></div>
+        <div class="tab">Projekty<i class="fas fa-briefcase"></i></div>
+        <div class="tab">Linki</div>
+        <div class="tab">Kontakt</div>
+        <div class="tab">ENG</div>        
+    </nav>
+</template>
+
+<script>
+    module.exports = {
+        name: 'NavbarComponent',
+        data(){
+            return {}
+        }
+    }
+</script>
+
+
+<style scoped lang="scss">
+    nav {
+        position: fixed;
+        // background-color: #2c3e50;
+        background-color: #212121;
+        display: flex;
+        flex-direction: column;
+        // justify-content: space-around;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        width: 150px;
+        .tab {
+            background-color: #212121;
+            height: 12vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column-reverse;
+            transition: all 0.5s;
+            cursor: pointer;
+            &:hover {
+                background-color: red;
+            }
+            i {
+                font-size: 3em;
+            }
+        }
+    }
+</style>
