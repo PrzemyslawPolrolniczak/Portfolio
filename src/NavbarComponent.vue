dc5842543d865b3dd90e71c9be4f67f69a36284d
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2018-04-27 00:02:50 +0200

    Colorfull navbar

--- src/NavbarComponent.vue
+++ src/NavbarComponent.vue
@@ -1,7 +1,15 @@
 <!--NavbarComponent.vue-->
 <template>
     <nav>
-        <div class="tab" v-for="tab in tabs" :key="tab.id">{{tab.text}}<i class="fas" :class="tab.icon"></i></div>        
+        <div class="tab"
+            :class="[tab.color, tab.isActive]"
+            @click="toggleActive(tab)" 
+            v-for="tab in tabs" 
+            :key="tab.id">
+            {{tab.text}}
+            <i class="fas" 
+                :class="[tab.icon]"></i>
+        </div>        
     </nav>
 </template>
 
@@ -11,13 +19,19 @@
         data(){
             return {
                 tabs: [
-                    {text: 'Intro', icon: 'fa-home'},
-                    {text: 'O mnie', icon: 'fa-user-circle'},
-                    {text: 'Projekty', icon: 'fa-briefcase'},
-                    {text: 'Linki', icon: 'fa-link'},
-                    {text: 'Kontakt', icon: 'fa-envelope'},
-                    {text: 'English', icon: 'fa-language'},                    
-                ]
+                    {text: 'Intro', icon: 'fa-home', color:'default', isActive: 'active'},
+                    {text: 'O mnie', icon: 'fa-user-circle', color:'blue', isActive: ''},
+                    {text: 'Projekty', icon: 'fa-briefcase', color:'red', isActive: ''},
+                    {text: 'Linki', icon: 'fa-link', color:'green', isActive: ''},
+                    {text: 'Kontakt', icon: 'fa-envelope', color:'yellow', isActive: ''},
+                    {text: 'English', icon: 'fa-language', color:'', isActive: ''},
+                ],
+                toggleActive(tab) {
+                    this.tabs.forEach(element => {
+                        element.isActive = '';
+                    });
+                    tab.isActive = 'active';
+                }
             }
         }
     }
@@ -47,7 +61,6 @@
             transition: all 0.5s;
             cursor: pointer;
             &:hover {
-                background-color: #212121;
                 padding-left: 20px;
             }
             &:last-of-type {
@@ -55,10 +68,54 @@
                 left: 0;
                 right: 0;
                 bottom: 0;
+                &.active {
+                    cursor: pointer;
+                    padding-left: 0;
+                    background-color: #31373c;
+                }
+                &:hover {
+                    padding-left: 0;
+                    background-color: #212121;
+                }
             }
             i {
                 font-size: 3em;
                 margin-bottom: 5px;
+            }
+            &.active {
+                cursor: initial;
+                padding-left: 20px;
+                background-color: #212121;
+                &.blue {
+                    color: #00B3FF; //55C1FF
+                }
+                &.red {
+                    color: #EF233C;
+                }
+                &.green {
+                    color: #91CB3E;
+                }
+                &.yellow {
+                    color: #F5B700;
+                }
+                &.default {
+                    color: #fff;
+                }
+            }
+            &.blue:hover {
+                color: #00B3FF;
+            }
+            &.red:hover {
+                color: #EF233C;
+            }
+            &.green:hover {
+                color: #91CB3E;
+            }
+            &.yellow:hover {
+                color: #F5B700;
+            }
+            &.default:hover {
+                color: #fff;
             }
         }
     }
